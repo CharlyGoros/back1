1afc83000eb8743acea8e7d1854e74b84644aea9
Gonzalo Rigoni <gonzalomiguelrigoni@gmail.com>
2024-05-26 23:12:30 -0300

    ad

--- back_flutter/lib/main.dart
+++ back_flutter/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Serverpod Demo',
+      title: 'Serverpodddddd Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
